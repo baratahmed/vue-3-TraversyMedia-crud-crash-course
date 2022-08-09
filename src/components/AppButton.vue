a8b46d1dfd195b5f307ab550e839f6ccad0b032d
baratahmed <baratahmed28@gmail.com>
2022-08-10 01:28:39 +0600

    first commit

---
+++ src/components/AppButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button  @click="onClick()" :style="{background: color}" class="btn">{{text}}</button>    
+</template>
+
+<script>
+    export default{
+        name: 'AppButton',
+        props: ['text','color'],
+        methods: {
+            onClick(){
+                this.$emit('btn-click')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTasks.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-for="(task, index) in tasks" :key="index">
+        <AppTask @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task',task.id)" :task="task"/>
+    </div>
+</template>
+
+<script>
+import AppTask from './AppTask.vue';
+    export default{
+    name: "AppTasks",
+    props: ["tasks"],
+    methods: {},
+    emits: ['delete-task','toggle-reminder'],
+    components: { AppTask }
+}
+</script>
+
+<style scoped>
+
+</style>
